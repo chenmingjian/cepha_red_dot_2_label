523150e67c220a12b392fe46fbefa56cf312b71a
chenmingjian <chen@chenmingjian.com>
2018-11-27 09:05:34 +0800

    the effort failed.

--- tester_for_divide.py
+++ tester_for_divide.py
@@ -1,25 +1,36 @@
+###############
+# sad warning #
+###############
+
 import os
 import numpy as np 
 
 ROOT = R'C:\Users\chen\Desktop\red_dot\label_tmp/'
 
-tmp1 = np.loadtxt(ROOT + '_2.txt')
-tmp2 = np.loadtxt(ROOT + '3.txt')
+txt_1 = np.loadtxt(ROOT + '_2.txt')
+txt_2 = np.loadtxt(ROOT + '3.txt')
 
-print(tmp1.shape, tmp2.shape)
+print(txt_1.shape, txt_2.shape)
 
-def get_some_point(tmp1):
-    tmp1_new_list = []
-    for i in tmp1: 
-        print(i)
-        if i[0] > 0.6 and i[1] > 0.7:
-            tmp1_new_list.append(i)
-    tmp1_new_list = np.array(tmp1_new_list)
-    return tmp1_new_list
+def get_some_point(txt_1, point_range):
+    distance_list = []
+    coordinate_list = []
+    for i in txt_1: 
+        tmp = (1-i[0])**2 + (1-i[1])**2 + \
+                i[0]**2 + i[1]**2  +\
+                (1-i[0])**2 + i[1]**2 +\
+                i[0]**2 + (1-i[1])**2
+        distance_list.append(tmp)
+        coordinate_list.append(i)
+    coordinate_list = np.array(coordinate_list)
+    sorted_distance_list = sorted(distance_list)
+    tmp = [distance_list.index(sorted_distance_list[i]) for i in range(point_range[0],point_range[1])]
+    return coordinate_list[tmp]
 
-tmp1 = get_some_point(tmp1)
-tmp2 = get_some_point(tmp2)
-print(tmp1.shape, tmp2.shape)
+_range = [25,35]
+txt_1 = get_some_point(txt_1,_range)
+txt_2 = get_some_point(txt_2,_range)
+print(txt_1.shape, txt_2.shape)
 
-np.savetxt(ROOT + '_2_p.txt',tmp1)
-np.savetxt(ROOT + '3_p.txt',tmp2)
+np.savetxt(ROOT + '_2_p.txt',txt_1)
+np.savetxt(ROOT + '3_p.txt',txt_2)
